62e502f0cd4509ac2be7bf1ecef0645c1bd8fc9e
Elif Yuzer <elifyuzer.12@gmail.com>
2026-04-16 15:07:42 +0200

    learned how install nodemon and nodemodules

---
+++ class-notes/04-NodeJs_Setup/index.js
@@ -0,0 +1,27 @@
+'use strict'
+/* --------------------- NodeJS ---------------------  */
+// Official: https://nodejs.org
+// Documents: https://nodejs.org/api
+
+/* -------------------- Node Shell -------------------- */
+
+// $ node
+// > console.log('Hello World')
+// > let moduleName = require("module")
+// > moduleName.builtinModules // Show included modules.
+// > .exit // > process.exit()
+
+/* --------------------- NodeJS ---------------------  */
+const node = 'NodeJs'
+console.log(node);
+
+/* -------------------- node -------------------- */
+
+// $ node index.js
+// $ node index
+// $ node . # default: index.js
+
+/* -------------------- nodeman -------------------- */
+// $ npm install -D nodemon -> 
+// $ npm i -g nodemon
+console.log('her degisklikte calisak bir yapi olusturuk nodemon ile');
